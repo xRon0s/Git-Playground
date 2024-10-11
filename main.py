289787cfa933d13b45640a22eab6d03c1ea921ca
tanaka <167485894+kariyasoma@users.noreply.github.com>
2024-10-11 10:18:36 +0900

    ボブが機能Bのスタブを作成

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import math
+import datetime
 
 # [機能A]@アリス担当 #############
 
 
 # [機能B]@ボブ担当 ###############
+def func_B():
+    print('func_B called.')
 
 
-# メイン処理 #####################
+# メイン処理 ####################
 if __name__ == "__main__":
 
-  print("start.")
+    print("start.")
 
-  # 機能Aの実行
+    # 機能Aの実行
 
-  # 機能Bの実行
+    # 機能Bの実行
+    func_B()
 
-  print("end.")
+    print("end.")
