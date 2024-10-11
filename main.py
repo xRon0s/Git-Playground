c4df82c27fae3250bc1a2e1f71ea14a0e7a7c111
tanaka <167485894+kariyasoma@users.noreply.github.com>
2024-10-11 10:25:46 +0900

    ボブが機能Bをリファクタリング

--- main.py
+++ main.py
@@ -6,16 +6,11 @@ import datetime
 
 # [機能B]@ボブ担当 ###############
 def func_B():
-    t1 = datetime.datetime.now()  # 現在時刻
-    t2 = datetime.datetime(2023, 11, 4, 10, 00, 00)  # 11/4 10:00
-    diff = t2 - t1
+    t = datetime.datetime(2023, 11, 4, 10, 00, 00)  # 11/4 10:00
+    diff = t - datetime.datetime.now()
     days = diff.days
     hours = diff.seconds // 3600
-    print('高専祭まで、あと', end='')
-    print(days, end='')
-    print('日と', end='')
-    print(hours, end='')
-    print('時間')
+    print(f'高専祭まで、あと{days}日と{hours}時間')
 
 
 # メイン処理 ####################
